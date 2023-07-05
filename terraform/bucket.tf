2df9ee7fd220d613fe92577201496fbfd829f215
toshit-dhote <toshit.dhote@gmail.com>
2023-07-05 19:51:42 +0530

    Update bucket.tf

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,5 +1,5 @@
 # Create a GCS Bucket
-resource "google_storage_bucket" "my_bucket" {
-name     = var.bucket_name
-location = var.region
-}
+#resource "google_storage_bucket" "my_bucket" {
+#name     = var.bucket_name
+#location = var.region
+#}
